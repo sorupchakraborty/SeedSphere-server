023f78141c9127829bcef3dd13ad88ddb6384ed8
sorup-chakraborty-17 <sorupchakraborty17@gmail.com>
2025-01-30 22:46:37 +0530

    Server connected. remove unnecessary blank lines in index.js

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ const PORT = 3000 || process.env.PORT;
 // MongoDB Database Connection
 
 const uri = process.env.MONGODB_URI;
-
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
