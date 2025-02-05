f3f2c55a4a2bf2f19c8eea709ef5ca2c09e16f29
sorup-chakraborty-17 <sorupchakraborty17@gmail.com>
2025-02-05 15:40:39 +0530

    update index

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ app.use(express.json());
 
 const PORT = 3000 || process.env.PORT;
 
-
-
 const uri = process.env.MONGODB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
